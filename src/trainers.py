93065b87ce717420f388713589ea64dc642feff4
Mees van de Walle <67998490+MvandeWalle@users.noreply.github.com>
2026-05-04 13:28:54 +0200

    Created DummyTrainer

--- src/trainers.py
+++ src/trainers.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import logging
 from splitter import Splitter
+from sklearn.dummy import DummyClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import RandomizedSearchCV, TimeSeriesSplit
 from xgboost import XGBClassifier
 from evaluator import Evaluator
@@ -23,6 +26,8 @@ class BaseTrainer:
         self.feature_cols = feature_cols
         self.year_col = year_col
         self.model = None  # child classes will set this
+
+        logger.info(f"{self.__class__.__name__} initialised.")
 
     def _get_fold_data(self, years: list[int]):
         # Filter data to a list of years, return X and y
@@ -108,6 +113,51 @@ class BaseTrainer:
         return final_results
 
 
+class DummyTrainer(BaseTrainer):
+    def __init__(
+        self,
+        data: pd.DataFrame,
+        target_col: str,
+        feature_cols: list[str],
+        year_col: str,
+    ):
+
+        super().__init__(data, target_col, feature_cols, year_col)
+        self.model = DummyClassifier(random_state=2026, strategy="prior")
+
+    def train_fold(self, fold_name: str, fold: dict):
+        X_train, y_train = self._get_fold_data(fold["train"])
+        X_val, y_val = self._get_fold_data(fold["val"])
+
+        # No hyperparameter tuning needed for DummyClassifier
+        self.model.fit(X_train, y_train)
+
+        prob_val = self.model.predict_proba(X_val)[:, 1]
+        val_results = pd.DataFrame(
+            {"y_true": y_val, "y_pred": prob_val, "Fold": fold_name}
+        )
+
+        # Empty params since there's nothing to tune
+        params_results = pd.DataFrame([{"Fold": fold_name}])
+
+        return val_results, params_results
+
+    def train_final(self, testing_data: pd.DataFrame):
+        if not hasattr(self, "best_params_per_fold"):
+            raise RuntimeError("train_final() must be called after run().")
+
+        X_train = self.data[self.feature_cols]
+        y_train = self.data[self.target_col]
+        X_test = testing_data[self.feature_cols]
+        y_test = testing_data[self.target_col]
+
+        # No hyperparameters to set for DummyClassifier
+        self.model.fit(X_train, y_train)
+        prob = self.model.predict_proba(X_test)[:, 1]
+
+        return pd.DataFrame({"y_true": y_test, "y_pred": prob})
+
+
 class RandomForestTrainer(BaseTrainer):
     def __init__(
         self,
@@ -127,8 +177,6 @@ class RandomForestTrainer(BaseTrainer):
         self.model = RandomForestClassifier(
             random_state=2026, class_weight="balanced", n_jobs=-1
         )
-
-        logging.debug("RF trainer initialised...")
 
 
 class XGBoostTrainer(BaseTrainer):
@@ -158,6 +206,7 @@ class LogisticRegressionTrainer(BaseTrainer):
         feature_cols: list[str],
         year_col: str,
     ):
+
         self.param_grid = {"C": [0.01, 0.1, 1, 10, 100]}
 
         super().__init__(data, target_col, feature_cols, year_col)
@@ -168,36 +217,48 @@ class LogisticRegressionTrainer(BaseTrainer):
 
 if __name__ == "__main__":
     logging.basicConfig(
-        level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
+        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
     )
     logger = logging.getLogger(__name__)
 
-    df = pd.read_csv("data/processed/train_binary_dataset.csv")
-    print(df.head())
-    fc = df.columns.drop(["date", "year", "wildfire"])
-    model_list = [XGBoostTrainer]
-    for m in model_list:
-        model = m(
-            data=df, target_col="wildfire", feature_cols=fc, year_col="year"
-        )
-        logging.debug("Model initialised.")
-        flds = Splitter(data=df, year_col="year")
-        v, p = model.run(folds=flds)
+    # Load training data and define features
+    df = pd.read_csv("data/processed/example_dataset.csv")
+    fc = df.columns.drop(["date", "year", "binary_wf", "numeric_wf"])
+
+    # Load  testing data 
+    testing_data = pd.read_csv("data/processed/test_dataset.csv")
+
+    # Define folds
+    folds = Splitter(data=df, year_col="year")
+
+    # Create an empty list to append the evaluation metrics of each model
+    metrics = []
+
+    # Train and evaluate
+    for TrainerClass in [DummyTrainer, RandomForestTrainer, XGBoostTrainer]:
+        logger.info(f"Running {TrainerClass.__name__}...")
+        model = TrainerClass(data=df, target_col="binary_wf", feature_cols=fc, year_col="year")
+        v, p = model.run(folds=folds)
         # evaluation = Evaluator(validation_results=v, best_parameters=p)
         # ev = evaluation.evaluate()
-        testing_data = pd.read_csv("data/processed/test_binary_dataset.csv")
+
         output = model.train_final(testing_data=testing_data)
 
         from sklearn.metrics import average_precision_score, brier_score_loss, f1_score
 
-        auprc = average_precision_score(y_true=output["y_true"], y_score=output["y_pred"])
+        auprc = average_precision_score(
+            y_true=output["y_true"], y_score=output["y_pred"]
+        )
         brier = brier_score_loss(y_true=output["y_true"], y_proba=output["y_pred"])
         f1 = f1_score(y_true=output["y_true"], y_pred=output["y_pred"].round())
 
         eval_metrics = {
+            "Model": TrainerClass.__name__,
             "AUPRC": round(auprc, 3),
             "Brier Score": round(brier, 3),
             "F1-score": round(f1, 3),
         }
-        
-        print(eval_metrics)
+
+        metrics.append(eval_metrics)
+    
+    print(pd.DataFrame(metrics))
